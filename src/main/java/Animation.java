94b88fd7761a30c26ef0e4314914426638a1437d
francescoryu <francescoryu04@gmail.com>
2022-08-25 23:00:19 +0200

    some changes

--- src/main/java/Animation.java
+++ src/main/java/Animation.java
@@ -1,39 +1,43 @@
 import java.awt.*;
+import java.util.concurrent.ThreadLocalRandom;
 import javax.swing.*;
 
 public class Animation extends JComponent {
     private int lastX = 0;
+    int min = 1;
+    int max = 20;
+    int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
     public Animation() {
-        Thread animationThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    repaint();
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
+        Thread animationThread = new Thread(() -> {
+            while (true) {
+                repaint();
+                try {
+                    Thread.sleep(randomNum);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
             }
         });
-
     animationThread.start();
     }
+
     public void paintComponent(Graphics g) {
         Graphics2D gg = (Graphics2D) g;
+        Graphics2D gg2 = (Graphics2D) g;
 
-        int w = getWidth();
-        int h = getHeight();
+        int w = 100;
+        int h = 30;
+        int s = 3;
+        int screenWidth = getWidth();
+        int screenHeight = getHeight();
+        int x = lastX + s;
 
-        int trainW = 100;
-        int trainH = 10;
-        int trainSpeed = 3;
-
-        int x = lastX + trainSpeed;
 
         gg.setColor(Color.BLACK);
-        gg.fillRect(x, h/2 + trainH, trainW, trainH);
+        gg.fillRect(x, screenHeight/2, w, h);
+
+        gg2.setColor(Color.BLUE);
+        gg2.fillRect(screenWidth/2, x, h, w);
 
         lastX = x;
     }
